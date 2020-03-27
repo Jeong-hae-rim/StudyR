da05177b81772c1b5f1ada8df0d6dc072776cd77
Jeong-hae-rim <58800295+Jeong-hae-rim@users.noreply.github.com>
2020-03-27 11:46:06 +0900

    updating
    행 단위로 읽어서 바꾸는 걸로 바꿈

--- textmining1.R
+++ textmining1.R
@@ -1,12 +1,10 @@
 
 
-words_utf8 <- scan("data/memo.txt", what="",encoding="UTF-8")
+words_utf8 <- readLines("data/memo.txt", encoding = 'UTF-8')
 words_utf8
 
 imsi1 <- gsub("[&$!#@%<>]","",words_utf8)
 imsi2 <- gsub("[1-9]","",imsi1)
 imsi3 <- gsub("[RAnalysisBigData]","",imsi2)
 
-text <- paste(imsi3, collapse = " ")
-
-write(text, file="memo_new.txt")
+write(imsi3, file="memo_new.txt")

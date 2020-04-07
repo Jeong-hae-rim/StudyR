e2d17c9e5d62d21836c751d7af47af4d70ea5423
Jeong-hae-rim <58800295+Jeong-hae-rim@users.noreply.github.com>
2020-04-07 15:09:08 +0900

    R web scrapping
    네이버 호텔 후기를 스크래핑 하여 txt 파일로 저장했다.

---
+++ naverhotel.R
@@ -0,0 +1,34 @@
+
+
+remDr <- remoteDriver(remoteServerAddr = "localhost" , port = 4445, browserName = "chrome")
+remDr$open()
+url<-'https://hotel.naver.com/hotels/item?hotelId=hotel:Shilla_Stay_Yeoksam&destination_kor=%EC%8B%A0%EB%9D%BC%EC%8A%A4%ED%85%8C%EC%9D%B4%20%EC%97%AD%EC%82%BC&rooms=2'
+remDr$navigate(url)
+Sys.sleep(3)
+pageLink <- NULL
+reple <- NULL
+curr_PageOldNum <- 0
+repeat{
+  doms <- remDr$findElements(using = "css selector", "div.review_desc")
+  Sys.sleep(1)
+  reple_v <- sapply(doms, function (x) {x$getElementText()})
+  print(reple_v)
+  reple <- append(reple, unlist(reple_v))
+  cat(length(reple), "\n")
+  pageLink <- remDr$findElements(using='css',"body > div > div.ng-scope > div.container.ng-scope > div.content > div.hotel_used_review.ng-isolate-scope > div.review_ta.ng-scope > div.paginate > a.direction.next")
+  remDr$executeScript("arguments[0].click();",pageLink)
+  last_page <- remDr$findElement(using='css', 'body > div > div.ng-scope > div.container.ng-scope > div.content > div.hotel_used_review.ng-isolate-scope > div.review_ta.ng-scope > div.paginate > a.direction.next.disabled')
+  Sys.sleep(2)
+  curr_PageElem <- remDr$findElement(using='css','body > div > div.ng-scope > div.container.ng-scope > div.content > div.hotel_used_review.ng-isolate-scope > div.review_ta.ng-scope > div.paginate > span:nth-child(2) > a')
+  curr_PageNewNum <- as.numeric(curr_PageElem$getElementText())
+  cat(paste(curr_PageOldNum, ':', curr_PageNewNum,'\n'))
+  
+  
+  if(pageLink==last_page)  {
+    cat("종료\n")
+    break; 
+  }
+  curr_PageOldNum <- curr_PageNewNum;
+}
+cat(length(reple), "개의 댓글 추출\n")
+write(reple,"hotel.txt")
